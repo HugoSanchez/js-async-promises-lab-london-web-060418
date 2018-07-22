25a7f165ff991365e6c5ef610d8ea39a8f1228b0
github username <github email address>
2018-07-22 12:05:58 +0100

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,49 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question) {
+  question = questions[0].questionText
+  document.querySelector('.question-container').innerHTML = question
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(question), time)
+  })
+}
+
+function removeQuestion() {
+  return new Promise((resolve, reject) => {
+    document.querySelector('.question-container').innerHTML = ''
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time)
+  .then(toggleTrueAndFalseButtons)
+  .then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  return document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(btn){
+    if (btn.classList.contains('hide')){
+      btn.classList.remove('hide')
+    } else {
+      btn.classList.add('hide')
+    }
+  })
+}
+
+function displayQuestionOnClick() {
+  let clickMe = document.querySelector('.waves-light')
+  return clickMe.addEventListener('click', () => {
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
